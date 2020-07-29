60a5cdb2897fd81ad3aac09ed39adcba65f7f34e
Jan Antonio Pereira <jan.antonio@ree.es>
2020-07-29 09:47:30 +0200

    001| Pruebas Home y Password

---
+++ src/pages/Password.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Password extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Resultado de Password</h1>
+      </div>
+    );
+  }
+}
